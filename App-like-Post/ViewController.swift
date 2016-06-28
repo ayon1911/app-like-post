8aa4d6f99f956c309946be283338c843f287d0c9
Khaled Rahman Ayon <khaledrahmanayon@khaleds-macbook-air.local>
2016-06-28 21:17:42 +0200

    Initial Commit

---
+++ App-like-Post/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App-like-Post
+//
+//  Created by Khaled Rahman Ayon on 28/06/16.
+//  Copyright © 2016 iosApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
